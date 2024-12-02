8d46dc0ac4455e6936be86b4406f5bb11072a941
Daniel Rein Cosare <reincosare07@gmail.com>
2024-12-03 02:08:50 +0800

    template na kulang ang contacts

--- app.py
+++ app.py
@@ -4,28 +4,23 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def index():
-    return render_template('index.html', title="Home Page", content="DSA - GROUP LABORATORY")
+def home():
+    return render_template('index.html')
 
 
 @app.route('/about')
 def about():
-    return render_template('about.html', title="About Page", content="This page contains information about our site.")
+    return render_template('about.html')
 
 
-@app.route('/profile')
-def profile():
-    return render_template('profile.html', title="Profile Page", content="Welcome to your profile!")
+@app.route('/works')
+def works():
+    return render_template('works.html')
 
 
-@app.route('/contact')
-def contact():
-    return render_template('contact.html', title="Contact Page", content="Get in touch with us via the contact form.")
-
-
-@app.route('/link list')
-def link_list():
-    return render_template('link list.html', title="Link List Page", content="link list ito")
+@app.route('/linked_list')
+def linked_list():
+    return render_template('linked_list.html')
 
 
 if __name__ == '__main__':
